83a07233d5427207aed33c5d74000379cb5be21e
tjrdbfl <tjrdbfl123@gmail.com>
2024-05-30 21:00:30 +0900

    feat - add main page

--- app/components/chat/module/chat-header.tsx
+++ app/components/chat/module/chat-header.tsx
@@ -9,7 +9,8 @@ const ChatHeader=()=>{
     <div className="flex justify-content-between mt-1">
     <SwitchAccessShortcutAddIcon className="chat_header_icon"/>
     <h1 className="chat_header_text">Turing Chat</h1>
-  </div>);
+  </div>
+  );
 };
 
 export default ChatHeader;
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,63 @@
-import * as React from 'react';
 import { NextPage } from 'next';
+import Link from 'next/link';
+import SwitchAccessShortcutAddIcon from '@mui/icons-material/SwitchAccessShortcutAdd';
 
 const Home: NextPage = () => {
 
+
   return (<>
-
-<div className='bg-[%001128] w-full h-full'>
-  <div className='flex justify-center items-center h-full'>
-  <div className='box relative w-[200px] h-[200px] bg-[#020223] overflow-hidden'></div>
+  <div className='flex flex-row h-full w-full 
+  bg-gradient-to-r from-blue-100 via-blue-400 to-violet-500'>
+  
+  <div className='h-full w-[60%] flex items-center'>
+  <div className='h-full w-full px-[17%] py-[9%] '>
+  <div className='animate-slidein300 opacity-0 box relative w-[700px] h-[700px] p-[2%] overflow-hidden'>
+  <img src="/main-page-icon.jpg"
+  alt="AI generated art, isometric"
+  className="animate-slidein300 opacity-0 rounded-3xl h-[100%]
+  "
+/>
   </div>
+  
+
 </div>
-    {/* <div className='flex flex-row w-full h-full'>
-      <div className='w-[50%] md:w-full flex justify-center items-center'>
-        <div className='box'/>
-      </div>
+</div>
+<a 
+  hidden={true}
+  href="https://kr.freepik.com/free-vector/ai-powered-content-creation-isometric-concept-with-chatbot-on-laptop-screen-3d-vector-illustration_43868977.htm#page=7&query=%EC%B1%97%EB%B4%87&position=16&from_view=keyword&track=ais_user&uuid=0652223a-52f0-4276-955b-fd7807ffa069">작가 macrovector</a>
+  
+  <div className='flex flex-col w-[40%] h-full'>
+  
+  <div className='flex flex-row mt-[25%]'>
+  <div className='flex flex-row w-full typing-demo-header'>
+  <SwitchAccessShortcutAddIcon className='text-[70px] mt-[27px] text-blue-700'/>
+  <div className=' text-7xl font-extrabold ml-[3%] md:leading-relaxed
+  bg-gradient-to-r from-blue-700 via-blue-700 to-violet-900 bg-clip-text inline-block text-transparent'>Turing ChatBot</div>
+  </div>
+  </div>
+  <div className='typing-demo-sub-header text-zinc-800 text-5xl font-semibold mt-[3%] ml-[5%]'>AI Chat bot</div>
+  <div className='animate-slidein700 opacity-0 text-3xl text-black mt-[5%] ml-[5%]'>
+  Turing Chat is a free-to-use AI system.
+  </div>
+  <div className='animate-slidein700 opacity-0 text-3xl text-black mt-[1%] leading-normal ml-[5%]'>
+  Use it for engaging conversations, gain insights
+  </div>
+  <div className='animate-slidein700 opacity-0 text-3xl text-black mt-[1%] leading-normal ml-[5%]'>
+  , automate tasks, and witness the future of AI!
+  </div>
 
-      <div className='lg:w-[50%] bg-[#04b0ee]'>
+  <Link 
+   href={'/pages/user/login'} 
+  className="animate-slidein900 border-2  flex items-center justify-center opacity-0 w-[40%] h-[8%] mt-[5%] ml-[5%] text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none dark:focus:ring-blue-800 font-medium rounded-xl text-3xl px-[2%] py-[2%] text-center me-2 mb-2
+  ">
+Getting started...
 
-      </div>
-     
-    </div> */}
+</Link>
+  
+  </div>
+ 
+  </div>
+
   </>
   );
 }
